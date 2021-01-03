4cc0d352bfade146e9779a0f5197c9a43b0a9778
jincangku <jincangku@protonmail.com>
2021-01-04 07:05:48 +1100

    Week03 作业

--- Week_03/Week03-04 全排列.py
+++
@@ -1,7 +0,0 @@
-class Solution(object):
-    def permute(self, nums):
-        """
-        :type nums: List[int]
-        :rtype: List[List[int]]
-        """
-        return permutations(nums, len(nums))
